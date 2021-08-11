b0eac4ed7fe4b58a21279da47faebdf3e869900e
Kerri-Leigh Grady <klgrady@gmail.com>
2021-08-11 10:30:10 -0400

    Trying again jebus

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv('../../.env')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nvjakxzsrsqont:8f2fb7d914222456dcba76358b954ac2beaf67c55e72537df610bc6991403e17@ec2-52-72-125-94.compute-1.amazonaws.com:5432/dd7ln5hctfhhv9'
 db = SQLAlchemy(app)
 
 class games(db.Model):
